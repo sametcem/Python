3775c5d3e453f78dc7816e4a21f56cd6f2f769e3
sametcem <50104561+sametcem@users.noreply.github.com>
2019-12-09 21:38:46 +0100

    Delete modules.py

--- hello/modules.py
+++
@@ -1,9 +0,0 @@
-# fonksiyonları tanımlıyoruz ve bunları baska yerde kullanailiyoruz -> INTERFACES
-
-def naber():
-    print("Hi")
-
-def mutlakdeger(numara):
-    if(numara <0):
-        return -numara
-    return numara
